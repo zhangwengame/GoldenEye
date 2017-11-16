72fdfb7d6ba5c699fc2c6afcf3a356a635fcbf6c
AndyZhang <wings@AndyZhangdeMacBook-Pro.local>
2017-11-16 14:10:32 -0800

    add timeout limit

---
+++ dumy.py
@@ -0,0 +1,7 @@
+import sys
+import time
+print(sys.argv)
+print(2)
+sys.stdout.flush()
+time.sleep(10)
+print(1)
---
+++ start.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+import subprocess
+from threading import Timer
+import os
+import signal
+def killAndPrint(p):
+	os.kill(p.pid, signal.SIGINT)
+	#p.kill()
+	#print("ahah "+p.stdout.read()+" test")
+def lambda_handler(event, context):
+	proc = subprocess.Popen("python goldeneye.py " + event['arg'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+	#proc = subprocess.Popen("python goldeneye.py http://www.google.com -s 1 -w 1", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+	#proc = subprocess.Popen(["python", "goldeneye.py", "http://127.0.0.1", "-s", "1", "-w", "1"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	kill_proc = killAndPrint
+	timer = Timer(5, kill_proc, [proc])
+	try:
+		timer.start()
+		stdout,stderr = proc.communicate()
+	finally:
+		timer.cancel()
+	return stdout + stderr
