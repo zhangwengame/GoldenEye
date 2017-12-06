bdd31bdd9f9b685fc6d52c6c39485f2c8f48c0e8
Lixiang Ao <aolixiang@gmail.com>
2017-12-05 17:54:59 -0800

    minor

--- start.py
+++ start.py
@@ -3,19 +3,25 @@ import subprocess
 from threading import Timer
 import os
 import signal
+import sys
+
 def killAndPrint(p):
 	os.kill(p.pid, signal.SIGINT)
 	#p.kill()
 	#print("ahah "+p.stdout.read()+" test")
 def lambda_handler(event, context):
-	proc = subprocess.Popen("python goldeneye.py " + event['arg'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+	proc = subprocess.Popen("python goldeneye.py " + event['args'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 	#proc = subprocess.Popen("python goldeneye.py http://www.google.com -s 1 -w 1", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 	#proc = subprocess.Popen(["python", "goldeneye.py", "http://127.0.0.1", "-s", "1", "-w", "1"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	kill_proc = killAndPrint
-	timer = Timer(5, kill_proc, [proc])
+	timer = Timer(event['duration'], kill_proc, [proc])
 	try:
 		timer.start()
 		stdout,stderr = proc.communicate()
 	finally:
 		timer.cancel()
 	return stdout + stderr
+
+
+if __name__=='__main__':
+    lambda_handler({'args': ' '.join(sys.argv[1:-1])})
